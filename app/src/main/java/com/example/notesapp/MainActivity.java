5569fa98c9174b947454dcc4771e6c4e4a092dcd
shaikhAbid <alishaikh4199@gmail.com>
2023-12-13 00:34:05 +0530

    First Commit

---
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.notesapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ExtendedFloatingActionButton fab;
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @SuppressLint("NotifyDataSetChanged")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.lisview);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("Notelist", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+
+        if (set == null) {
+            notes.add("Example note");
+        } else {
+            notes = new ArrayList(set);
+        }
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Going from MainActivity to NotesEditorActivity
+                Intent intent = new Intent(getApplicationContext(), Notess.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+                // To delete the data from the App
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                                HashSet<String> set = new HashSet(MainActivity.notes);
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                            }
+                        }).setNegativeButton("No", null).show();
+                return true;
+            }
+        });
+
+        fab = findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Notess.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/notesapp/Notess.java
@@ -0,0 +1,72 @@
+package com.example.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.HashSet;
+
+public class Notess extends AppCompatActivity {
+    EditText editText;
+    ImageView back;
+    int noteId;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notess);
+
+        editText=findViewById(R.id.editText);
+
+        Intent intent=getIntent();
+        noteId = intent.getIntExtra("noteId", -1);
+        if (noteId != -1) {
+            editText.setText(MainActivity.notes.get(noteId));
+        } else {
+
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() - 1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+
+        }
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // add your code here
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.notes.set(noteId, String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // add your code here
+            }
+        });
+
+        back=findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(Notess.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
